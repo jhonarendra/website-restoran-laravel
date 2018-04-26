aaa91c3f17e702793fc6580c9581492a4ab5f8da
OgikParama <Paramasutha26@gmail.com>
2018-04-26 22:27:03 +0800

    penambahan dashboard pada halaman pegawai sama perbaikin desain halaman pegawai dikit

--- resources/views/pegawai/dashboard/index.blade.php
+++ resources/views/pegawai/dashboard/index.blade.php
@@ -20,9 +20,9 @@
                 </div>
             </div>
             <ul class="list-group">
-                <a href="{{URL('pegawai')}}" class="list-group-item list-group-item-action active">Dashboard</a>
+                <a href="{{URL('pegawai/dashboard')}}" class="list-group-item list-group-item-action">Dashboard</a>
                 <a href="{{URL('pegawai/reservasi')}}" class="list-group-item list-group-item-action">Reservasi</a>
-                <a href="{{URL('pegawai/pemesanan')}}" class="list-group-item list-group-item-action">Pemesanan</a>
+                <a href="{{URL('pegawai/pemesanan')}}" class="list-group-item list-group-item-action active">Pemesanan</a>
                 <a href="{{URL('pegawai/pelanggan')}}" class="list-group-item list-group-item-action">Pelanggan</a>
                 <a href="{{URL('pegawai/pegawai')}}" class="list-group-item list-group-item-action">Pegawai</a>
                 <a href="{{URL('pegawai/restoran')}}" class="list-group-item list-group-item-action">Restoran</a>
@@ -32,7 +32,7 @@
         </div>
         <div class="col-md-9">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">ADMIN</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -42,35 +42,30 @@
                     @endif
 					<div class="row align-items-center">
 						<div class="col-6 mx-auto col-md-6 order-md-2">
-							<div class="card-body">
-								<div style="text-align: center">
-									<img class="img-fluid mb-3 mb-md-0" src="" alt="" width="1024" height="860">
-									<h4 class="card-title">
-										{{$pegawai['nama_pegawai']}}
-									</h4>
-									<p class="card-text">
-										{{$pegawai['email_pegawai']}}<br />
-										{{$pegawai['jabatan_pegawai']}}
-									</p>
-								</div>
-							</div>
+							<img class="img-fluid mb-3 mb-md-0" src="" alt="" width="1024" height="860">
 						</div>
 						<div class="col-md-6 order-md-1 text-center text-md-left pr-md-5">
-							<h1 class="mb-3 bd-text-purple-bright">Berita Terbaru</h1>
+							<h1 class="mb-3 bd-text-purple-bright">NEWS</h1>
 							<p class="lead">
-								Contoh news Contoh news Contoh news Contoh newsContoh newsContoh newsContoh newsContoh newsContoh newsContoh newsContoh newsContoh newsContoh newsContoh newsContoh newsContoh newsContoh newsContoh newsContoh newsContoh newsContoh newsContoh newsContoh newsContoh newsContoh newsContoh newsContoh news 
+								CONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWS
+							</p>
+							<p class="lead mb-4">
+								CONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWSCONTOH NEWS.
 							</p>
 							<div class="d-flex flex-column flex-md-row lead mb-3">
-								<a href="" class="btn btn-lg btn-outline-secondary">Download Berkas</a>
+								<a href="" class="btn btn-lg btn-bd-primary mb-3 mb-md-0 mr-md-3" >Get started</a>
+								<a href="" class="btn btn-lg btn-outline-secondary" >Download</a>
 							</div>
 							<p class="text-muted mb-0">
-								Pegawai Adminstrator
+								ADMIN CONTOH
 							</p>
 						</div>
 					</div>
 				</div>
-			</div>
-		</div>
-	</div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
+
+			
